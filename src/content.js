52933943d833bde97e08a1c7d9c5aef2c7a65128
Crystal Danger <cdneal091@gmail.com>
2019-09-26 20:23:50 -0600

    STRONK PEENK

--- src/content.js
+++ src/content.js
@@ -1,11 +1,11 @@
 export const content = [
   {
-    text: 'In Memorium',
+    text: '<pink>In</pink> Memorium',
     image_link: 'https://i.pinimg.com/originals/5a/80/93/5a80933b8acffb1cfc55398e351d0a36.png',
     display_time: 4
   },
   {
-    text: 'yeet yeet',
+    text: 'yeet <strong>yeet</strong>',
     image_link: 'https://ichef.bbci.co.uk/news/660/cpsprodpb/13F00/production/_95146618_bills.jpg',
     display_time: 10
   }
